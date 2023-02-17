cbeeefa0dab6060a6b35a1b766bbe73b9115c124
Bade Vikas Vasudeo <119315927+VikasPrograms@users.noreply.github.com>
2023-02-17 13:12:56 +0530

    Update Stack_4.cpp

--- Stack_4.cpp
+++ Stack_4.cpp
@@ -4,7 +4,7 @@
 // Input         :  Int
 // Output        :  Int
 // Author        :  Vikas Bade
-// Date          :  18 May 2021
+// Date          :  24 Jully 2022
 //
 //----------------------------------------------------------------------
 
@@ -46,7 +46,7 @@ class Stack
 //
 //
 // Class Name       :   Stack
-// Function Date    :   18 May 2021
+// Function Date    :   24 Jully 2022
 // Function Author  :   Vikas Bade
 // Parameters       :   NONE
 // Description      :   This Is Constructor Of Class Stack
@@ -67,7 +67,7 @@ Stack :: Stack() // constructor
 //
 //
 // Class Name       :   ~Stack
-// Function Date    :   18 May 2021
+// Function Date    :   24 Jully 2022
 // Function Author  :   Vikas Bade
 // Parameters       :   NONE
 // Description      :   This Is Distructor Of Class Stack
@@ -86,7 +86,7 @@ Stack :: ~Stack()
 //
 //
 // Function Name    :   Push()
-// Function Date    :   18 May 2021
+// Function Date    :   24 Jully 2022
 // Function Author  :   Vikas Bade
 // Parameters       :   int 
 // Description      :   Push Function Add New Node / Element At Top Of A Stack 
@@ -111,7 +111,7 @@ void Stack :: Push(int iNo) // insertfirst()
 //
 //
 // Function Name    :   Pop()
-// Function Date    :   18 May 2021
+// Function Date    :   24 Jully 2022
 // Function Author  :   Vikas Bade
 // Parameters       :   -- 
 // Description      :   Pop Function Remove Node / Element From Top Of A Stack 
@@ -146,7 +146,7 @@ int Stack :: Pop() // delete first
 //
 //
 // Function Name    :   Display()
-// Function Date    :   18 May 2021
+// Function Date    :   24 Jully 2022
 // Function Author  :   Vikas Bade
 // Parameters       :   ----
 // Description      :   Display Function Display The Nodes / Elements In Stack 
@@ -172,7 +172,7 @@ void Stack :: Display()
 //
 //
 // Function Name    :   Count()
-// Function Date    :   18 May 2021
+// Function Date    :   24 Jully 2022
 // Function Author  :   Vikas Bade
 // Parameters       :   ----
 // Description      :   Count Function Count Number Of Nodes / Elements In The Stack 
@@ -192,7 +192,7 @@ int Stack :: Count()
 //
 //
 // Function Name   :   Entry Point Function
-// Function Date   :   18 May 2021
+// Function Date   :   24 Jully 2022
 // Function Author  :   Vikas Bade
 // Brif Description :  Function Calls For Above Created Functions With Dynamic Input
 //
